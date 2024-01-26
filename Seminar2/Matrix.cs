f83ce273ee8dee3bf775623d4bf665762f27b7db
Asravin <maisweedfell@gmail.com>
2024-01-26 11:18:54 +0400

    Печать матрицы

---
+++ Seminar2/Matrix.cs
@@ -0,0 +1,26 @@
+﻿internal class Matrix
+{
+    private static void Main(string[] args)
+    {
+        int num = Convert.ToInt32(Console.ReadLine());
+        int[,] matrix = new int[num, num];
+        for (int i = 0; i < num; i++)
+        {
+            for (int j = 0; j < num; j++)
+            {
+                matrix[i, j] = (i + 1) * (j + 1);
+                matrix[i, j] = (i + 1) * (j + 1);
+            }
+            Console.WriteLine();
+        }
+        for (int i = 0; i < num; i++)
+        {
+            for (int j = 0; j < num; j++)
+            {
+                Console.Write(matrix[i, j]);
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
