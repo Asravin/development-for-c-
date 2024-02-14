23c30ed0d7b40df067a2b423669eba8f89054a17
Asravin <maisweedfell@gmail.com>
2024-02-14 15:02:39 +0200

    Метод сортировки выбором

---
+++ SelectionSort/Program.cs
@@ -0,0 +1,18 @@
+﻿void SortSelection(int[] collection)
+    {
+    int size = collection.Lenght;
+    for(int i = 0; i < size - 1; i++)
+    {
+        int pos = i;
+        for(int j = i + 1; j < size - 1; i++)
+        {
+            if(collection[j] < collection[pos])
+            {
+                pos = j;
+            } 
+        }
+        int temp = collection[i];
+        collection[i] = collection[pos];
+        collection[pos] = temp
+    }
+}
